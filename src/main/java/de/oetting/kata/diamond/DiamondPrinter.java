050e9632edc799955d15250decae1f8ccc54dcb1
Hinnerk Oetting <hinnerk.oetting@consileon.de>
2015-03-27 19:33:27 +0100

    removed unused methods

--- src/main/java/de/oetting/kata/diamond/DiamondPrinter.java
+++ src/main/java/de/oetting/kata/diamond/DiamondPrinter.java
@@ -3,17 +3,17 @@ package de.oetting.kata.diamond;
 public class DiamondPrinter {
 
 	public String print(char character) {
-		int numberOfCharacters = character - 'A' + 1;
-		String upperHalf = createLinesUntilWidestPoint(character, numberOfCharacters);
-		String lowerHalf = createStringUntilBottom(numberOfCharacters);
+		int numberOfDiffferentCharacters = character - 'A' + 1;
+		String upperHalf = createLinesUntilWidestPoint(character, numberOfDiffferentCharacters);
+		String lowerHalf = createStringUntilBottom(numberOfDiffferentCharacters);
 		return upperHalf + lowerHalf;
 	}
 
 	private String createStringUntilBottom(int maxLengthOfDiamond) {
 		String result = "";
 		for (int i = maxLengthOfDiamond - 2; i >= 0; i--) {
-			result += createOutputLine((char) ('A' + i), maxLengthOfDiamond);
-			if ((char) ('A' + i) != 'A')
+			result += createOutputLine(createNthCharacter(i), maxLengthOfDiamond);
+			if (createNthCharacter(i) != 'A')
 				result += '\n';
 		}
 		return result;
@@ -22,11 +22,15 @@ public class DiamondPrinter {
 	private String createLinesUntilWidestPoint(char character, int numberOfCharacters) {
 		String result = "";
 		for (int i = 0; i < numberOfCharacters; i++) {
-			result += createOutputLine((char) ('A' + i), numberOfCharacters);
+			result += createOutputLine(createNthCharacter(i), numberOfCharacters);
 			if (character != 'A')
 				result += '\n';
 		}
 		return result;
+	}
+
+	private char createNthCharacter(int i) {
+		return (char) ('A' + i);
 	}
 
 	private String createOutputLine(char c, int numberOfCharacters) {
@@ -36,25 +40,18 @@ public class DiamondPrinter {
 	}
 
 	private String createOutputLineForNonA(char c, int numberOfCharacters) {
-		String spacesBefore = timesSpace(numberOfCharacters - (c- 'A') - 1);
-		String characterPlusSpacesBetween = c + timesSpace((c - 'A') * 2 - 1) + c;
+		String spacesBefore = createNumberOfSpacesSpaces(numberOfCharacters - (c- 'A') - 1);
+		String characterPlusSpacesBetween = c + createNumberOfSpacesSpaces((c - 'A') * 2 - 1) + c;
 		return spacesBefore + characterPlusSpacesBetween;
 	}
 
-	private String createTextForC(int numberOfCharacters) {
-		return timesSpace(numberOfCharacters - 3) + "C" + timesSpace(3) + "C";
-	}
-
-	private String createTextForB(int numberOfCharacters) {
-		return timesSpace(numberOfCharacters - 2) + 'B' + timesSpace('B' - 'A') + 'B';
-	}
-
+	//This is a special method for A because this will be the only line which only contains one character
 	private String createTextForA(int numberOfCharacters) {
-		String spaces = timesSpace(numberOfCharacters - 1);
+		String spaces = createNumberOfSpacesSpaces(numberOfCharacters - 1);
 		return spaces + "A";
 	}
 
-	private String timesSpace(int times) {
+	private String createNumberOfSpacesSpaces(int times) {
 		String result = "";
 		for (int i = 0; i < times; i++)
 			result += ' ';
