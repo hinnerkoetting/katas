7a0a60180db2e7b95149488f96b4bc8ec2109196
Hinnerk Oetting <hinnerk.oetting@consileon.de>
2015-03-27 18:01:27 +0100

    works now for A and B

--- src/main/java/de/oetting/kata/diamond/DiamondPrinter.java
+++ src/main/java/de/oetting/kata/diamond/DiamondPrinter.java
@@ -2,8 +2,28 @@ package de.oetting.kata.diamond;
 
 public class DiamondPrinter {
 
-	public String print(String string) {
-		return "A";
+	public String print(Character character) {
+		String result = "";
+		int maxLengthOfDiamond = character - 'A' + 1;
+		if (character.equals('A')) {
+			result += print('A', 1);
+		} else {
+			result += print('A', maxLengthOfDiamond) + '\n';
+			result += print('B', maxLengthOfDiamond) + '\n';
+			result += print('A', maxLengthOfDiamond);
+		}
+		return result;
+	}
+
+	private String print(char c, int maxLengthOfDiamond) {
+		if (c == 'A') {
+			if (maxLengthOfDiamond == 1)
+				return "A";
+			else
+				return " A";
+		} else {
+			return "B B";
+		}
 	}
 
 }
--- src/test/java/de/oetting/kata/diamond/DiamondPrinterTest.java
+++ src/test/java/de/oetting/kata/diamond/DiamondPrinterTest.java
@@ -8,11 +8,17 @@ public class DiamondPrinterTest {
 
 	@Test
 	public void create_characterA_returnsA() {
-		String output = whenCreating("A");
+		String output = whenCreating('A');
 		assertEquals("A", output);
 	}
+	
+	@Test
+	public void create_characterB_returnsDiamongWithB() {
+		String output = whenCreating('B');
+		assertEquals(" A\nB B\n A", output);
+	}
 
-	private String whenCreating(String string) {
-		return new DiamondPrinter().print(string);
+	private String whenCreating(char character) {
+		return new DiamondPrinter().print(character);
 	}
 }
