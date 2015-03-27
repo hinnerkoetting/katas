3f4d6895c3617c9689c14d511ffc4f2af58f5bfa
Hinnerk Oetting <hinnerk.oetting@consileon.de>
2015-03-27 19:39:52 +0100

    refactoring

--- src/main/java/de/oetting/kata/diamond/DiamondPrinter.java
+++ src/main/java/de/oetting/kata/diamond/DiamondPrinter.java
@@ -3,35 +3,35 @@ package de.oetting.kata.diamond;
 public class DiamondPrinter {
 
 	public String print(char character) {
-		int numberOfDiffferentCharacters = character - 'A' + 1;
-		String upperHalf = createUpperHalf(character, numberOfDiffferentCharacters);
-		String middleLine = createMiddleLine(numberOfDiffferentCharacters);
-		String lowerHalf = createLowerHalf(numberOfDiffferentCharacters);
+		String upperHalf = createUpperHalf(character);
+		String middleLine = createMiddleLine(character);
+		String lowerHalf = createLowerHalf(character);
 		return upperHalf + middleLine + lowerHalf;
 	}
 
-	private String createMiddleLine(int numberOfDiffferentCharacters) {
-		String line = createOutputLine(createNthCharacter(numberOfDiffferentCharacters - 1), numberOfDiffferentCharacters);
-		if (numberOfDiffferentCharacters == 1) 
-			return line;
-		return line + '\n';
-	}
-
-	private String createLowerHalf(int maxLengthOfDiamond) {
+	private String createUpperHalf(char character) {
 		String result = "";
-		for (int i = maxLengthOfDiamond - 2; i >= 0; i--) {
-			result += createOutputLine(createNthCharacter(i), maxLengthOfDiamond);
-			if (createNthCharacter(i) != 'A')
+		for (int i = 0; i < getNumberOfDifferentCharacters(character) - 1; i++) {
+			result += createOutputLine(createNthCharacter(i), getNumberOfDifferentCharacters(character));
+			if (character != 'A')
 				result += '\n';
 		}
 		return result;
 	}
 
-	private String createUpperHalf(char character, int numberOfCharacters) {
+	private String createMiddleLine(char character) {
+		String line = createOutputLine(createNthCharacter(getNumberOfDifferentCharacters(character) - 1),
+				getNumberOfDifferentCharacters(character));
+		if (getNumberOfDifferentCharacters(character) == 1)
+			return line;
+		return line + '\n';
+	}
+
+	private String createLowerHalf(char character) {
 		String result = "";
-		for (int i = 0; i < numberOfCharacters - 1; i++) {
-			result += createOutputLine(createNthCharacter(i), numberOfCharacters);
-			if (character != 'A')
+		for (int i = getNumberOfDifferentCharacters(character) - 2; i >= 0; i--) {
+			result += createOutputLine(createNthCharacter(i), getNumberOfDifferentCharacters(character));
+			if (createNthCharacter(i) != 'A')
 				result += '\n';
 		}
 		return result;
@@ -48,12 +48,13 @@ public class DiamondPrinter {
 	}
 
 	private String createOutputLineForNonA(char c, int numberOfCharacters) {
-		String spacesBefore = createNumberOfSpacesSpaces(numberOfCharacters - (c- 'A') - 1);
+		String spacesBefore = createNumberOfSpacesSpaces(numberOfCharacters - (c - 'A') - 1);
 		String characterPlusSpacesBetween = c + createNumberOfSpacesSpaces((c - 'A') * 2 - 1) + c;
 		return spacesBefore + characterPlusSpacesBetween;
 	}
 
-	//This is a special method for A because this will be the only line which only contains one character
+	// This is a special method for A because this will be the only line which
+	// only contains one character
 	private String createTextForA(int numberOfCharacters) {
 		String spaces = createNumberOfSpacesSpaces(numberOfCharacters - 1);
 		return spaces + "A";
@@ -64,6 +65,10 @@ public class DiamondPrinter {
 		for (int i = 0; i < times; i++)
 			result += ' ';
 		return result;
+	}
+
+	private int getNumberOfDifferentCharacters(char character) {
+		return character - 'A' + 1;
 	}
 
 }
