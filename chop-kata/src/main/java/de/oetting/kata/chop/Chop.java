51d15667d0024d7fdcbf48fc1f80219b715f0d46
Hinnerk Oetting <hinnerk.oetting@consileon.de>
2015-04-02 17:17:32 +0200

    added first binary search in upper half

--- chop-kata/src/main/java/de/oetting/kata/chop/Chop.java
+++ chop-kata/src/main/java/de/oetting/kata/chop/Chop.java
@@ -4,12 +4,32 @@ import java.util.List;
 
 public class Chop {
 
-	public <S> int find(List<S> list, S element) {
+	public <S extends Comparable<S>> int find(List<S> list, S element) {
 		if (list.isEmpty())
 			return -1;
+		int middle = list.size() / 2;
+		int comparison = list.get(middle).compareTo(element);
+		if (comparison == 0)
+			return middle;
+		if (comparison < 0)
+			return findInBiggerHalf(list, element, middle);
 		if (list.get(0) == element)
 			return 0;
 		return -1;
 	}
 
+	private <S extends Comparable<S>> int findInBiggerHalf(List<S> list, S element, int pivot) {
+		if (list.size() <= pivot + 1) {
+			return -1;
+		}
+		int newPivot = (list.size() + pivot) / 2;
+		int comparison = list.get(newPivot).compareTo(element);
+		if (comparison == 0)
+			return newPivot;
+		if (comparison < 0) {
+			return findInBiggerHalf(list, element, newPivot);
+		}
+		return -1;
+
+	}
 }
--- chop-kata/src/test/java/de/oetting/kata/chop/ChopTest.java
+++ chop-kata/src/test/java/de/oetting/kata/chop/ChopTest.java
@@ -3,6 +3,8 @@ package de.oetting.kata.chop;
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.Test;
@@ -30,12 +32,34 @@ public class ChopTest {
 		int position = whenFindingElement(2);
 		assertEquals(-1, position);
 	}
+	
+	@Test
+	public void find_elementInListWithTwoElements_returnsElementsPosition() {
+		givenListContains(1, 2);
+		int position = whenFindingElement(2);
+		assertEquals(1, position);
+	}
+	
+	@Test
+	public void find_topElementInListWithThreeElements_returnsElementsPosition() {
+		givenListContains(1, 2, 3);
+		int position = whenFindingElement(3);
+		assertEquals(2, position);
+	}
+	
+	@Test
+	public void find_topElementInListWithFifeElements_returnsElementsPosition() {
+		givenListContains(1, 2, 3, 4, 5);
+		int position = whenFindingElement(5);
+		assertEquals(4, position);
+	}
 
-	private void givenListContains(int i) {
-		list.add(i);
+	private void givenListContains(Integer... numbers) {
+		list.addAll(Arrays.asList(numbers));
 	}
 
 	private int whenFindingElement(int i) {
+		Collections.sort(list);
 		return new Chop().find(list, i);
 	}
 }
