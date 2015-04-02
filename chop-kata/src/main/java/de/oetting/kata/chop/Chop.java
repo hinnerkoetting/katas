8446e9967f0841106f50fd375b315972b0475d3d
Hinnerk Oetting <hinnerk.oetting@consileon.de>
2015-04-02 17:55:36 +0200

    Refactoring to remove duplication

--- chop-kata/src/main/java/de/oetting/kata/chop/Chop.java
+++ chop-kata/src/main/java/de/oetting/kata/chop/Chop.java
@@ -5,18 +5,10 @@ import java.util.List;
 public class Chop {
 
 	public <S extends Comparable<S>> int find(List<S> list, S element) {
-		if (list.isEmpty())
-			return -1;
-		int middle = list.size() / 2;
-		int comparison = list.get(middle).compareTo(element);
-		if (comparison == 0)
-			return middle;
-		if (comparison < 0)
-			return find(list, element, middle + 1, middle + 1, list.size() - 1);
-		return find(list, element, middle, 0, middle - 1);
+		return find(list, element, 0, list.size() - 1);
 	}
 
-	private <S extends Comparable<S>> int find(List<S> list, S element, int pivot, int min, int max) {
+	private <S extends Comparable<S>> int find(List<S> list, S element, int min, int max) {
 		int newPivot = (max + min) / 2;
 		if (newPivot < min)
 			return -1;
@@ -26,7 +18,7 @@ public class Chop {
 		if (comparison == 0)
 			return newPivot;
 		if (comparison < 0)
-			return find(list, element, newPivot + 1, newPivot + 1, max);
-		return find(list, element, newPivot - 1, min, newPivot - 1);
+			return find(list, element, newPivot + 1, max);
+		return find(list, element, min, newPivot - 1);
 	}
 }
