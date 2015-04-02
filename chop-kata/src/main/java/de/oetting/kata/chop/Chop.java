a6808479d6a6b3ee950ecdf15486c84616452ff6
Hinnerk Oetting <hinnerk.oetting@consileon.de>
2015-04-02 16:59:37 +0200

    added test for list with one element

--- chop-kata/src/main/java/de/oetting/kata/chop/Chop.java
+++ chop-kata/src/main/java/de/oetting/kata/chop/Chop.java
@@ -5,6 +5,10 @@ import java.util.List;
 public class Chop {
 
 	public <S> int find(List<S> list, S element) {
+		if (list.isEmpty())
+			return -1;
+		if (list.get(0) == element)
+			return 0;
 		return -1;
 	}
 
--- chop-kata/src/test/java/de/oetting/kata/chop/ChopTest.java
+++ chop-kata/src/test/java/de/oetting/kata/chop/ChopTest.java
@@ -16,6 +16,24 @@ public class ChopTest {
 		int position = whenFindingElement(1);
 		assertEquals(-1, position);
 	}
+	
+	@Test
+	public void find_elementInlistWithOnlyThisElement_returns0() {
+		givenListContains(1);
+		int position = whenFindingElement(1);
+		assertEquals(0, position);
+	}
+	
+	@Test
+	public void find_elementInlistWithOnlyAnotherElement_returnsMinusOne() {
+		givenListContains(1);
+		int position = whenFindingElement(2);
+		assertEquals(-1, position);
+	}
+
+	private void givenListContains(int i) {
+		list.add(i);
+	}
 
 	private int whenFindingElement(int i) {
 		return new Chop().find(list, i);
