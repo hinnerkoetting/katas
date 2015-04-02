a2720603f1f4b02a72a540e66e42628202aa68ce
Hinnerk Oetting <hinnerk.oetting@consileon.de>
2015-04-02 17:59:12 +0200

    Refactoring: Extracted Methods

--- chop-kata/src/main/java/de/oetting/kata/chop/Chop.java
+++ chop-kata/src/main/java/de/oetting/kata/chop/Chop.java
@@ -15,10 +15,26 @@ public class Chop {
 		if (pivotElement > maxIndex)
 			return -1;
 		int comparison = list.get(pivotElement).compareTo(element);
-		if (comparison == 0)
+		if (elementWasFound(comparison))
 			return pivotElement;
-		if (comparison < 0)
-			return find(list, element, pivotElement + 1, maxIndex);
+		if (searchedElementIsBiggerThanPivotElement(comparison))
+			return findBetweenPivotAndMax(list, element, maxIndex, pivotElement);
+		return findBetweenMinAndPivot(list, element, minIndex, pivotElement);
+	}
+
+	private <S extends Comparable<S>> int findBetweenMinAndPivot(List<S> list, S element, int minIndex, int pivotElement) {
 		return find(list, element, minIndex, pivotElement - 1);
+	}
+
+	private <S extends Comparable<S>> int findBetweenPivotAndMax(List<S> list, S element, int maxIndex, int pivotElement) {
+		return find(list, element, pivotElement + 1, maxIndex);
+	}
+
+	private boolean searchedElementIsBiggerThanPivotElement(int comparison) {
+		return comparison < 0;
+	}
+
+	private boolean elementWasFound(int comparison) {
+		return comparison == 0;
 	}
 }
